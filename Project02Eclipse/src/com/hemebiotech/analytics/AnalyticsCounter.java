432d8f663b7cb4eb47420624e32b6a72a4430ab1
Thirmdind5 <pawan.bhojanala@outlook.com>
2020-10-28 04:15:33 -0400

    The program is complete as per the requirements of project.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,113 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * AnalyticsCounter class is the main class of this program. In the main method an instance of the class is created.
+ * This class uses another class ReadSymptomDataFromFile. This is to read data from a file.
+ */
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	String path = "D:\\symptoms.txt";
+	private	HashMap<String,Integer> symptomsCounter = new HashMap<String,Integer>();
+	
+	public AnalyticsCounter() {
+		System.out.println("In the constructor");
+	}
+	public static void main(String args[])  {
+		
+		AnalyticsCounter counter = new AnalyticsCounter();
+	
+		
+		counter.saveToFile(counter.occurrencesList(counter.alphabetizeList(counter.getSymptoms())));
+		
+		
+				
+		}//closing for the main method
+		
+			
+/**
+ * getSymptoms Method
+ * This method creates an instance of the "ReadSymptomDataFromFile" class
+ * and returns a list of the symptoms. 
+ * 
+ */
+	List<String> getSymptoms(){
+		
+		
+		ReadSymptomDataFromFile symptomReader = new ReadSymptomDataFromFile(path);
+		
+		return symptomReader.GetSymptoms();
+	}
+	
+	/**
+	 * alphabetizeList method
+	 * This method takes in a list of symptoms and returns the list in alphabetical order
+	 * 
+	 */
+	
+	List<String> alphabetizeList(List<String> symptomsList){
+		
+		Collections.sort(symptomsList);
+		
+		return symptomsList;
+	}//end of alphabeteizeList
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+	
+	/**
+	 * occurrencesList Method
+	 * This method takes the list of occurrences and generates a list of symptoms and number of 
+	 * occurences
+	 */
+	HashMap<String,Integer> occurrencesList(List<String> symptomsList) {
+		
+		
+		
+		for(String s: symptomsList) {
+			
+			symptomsCounter.put(s, symptomsCounter.getOrDefault(s, 0) + 1);
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
-}
+		return symptomsCounter;
+		
+	}//end of occurrencesList
+	
+	/**
+	 * saveToFile Method
+	 * This method saves the information to a file. 
+	 * This method consumes a HashMap and creates a file.
+	 */
+	
+	void saveToFile(HashMap<String,Integer> Map) {
+		
+		
+		try {
+	         BufferedWriter out = new BufferedWriter(new FileWriter("D:\\outfilename"));
+	         
+	         for( HashMap.Entry<String,Integer> me : symptomsCounter.entrySet()){
+	 			
+	 			out.append(me.getKey() + " : " + me.getValue());
+	 			out.newLine();
+	 		 		
+	 		   }
+	         	         
+	         out.close();
+	         System.out.println("File created successfully");
+	      }
+	      catch (IOException e) {
+	    	  System.out.println(e.toString());
+	      }
+	}// End of saveToFile method
+	
+}// End of Class 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application
+
+This program needs the symptoms.txt file to read the symptoms. For my project I copied it to my local drive. To run the program,
+the user will need to update the location of the file.
+This program creates a file with symptoms and the number of occurrences. The output location of the file also needs to be uddated when 
+running the program.
