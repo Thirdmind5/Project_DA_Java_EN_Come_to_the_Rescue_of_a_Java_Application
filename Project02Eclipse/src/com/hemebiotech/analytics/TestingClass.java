316a9f880bc804ca8eb3f6a23d219ca7c1c13eae
Thirdmind5 <pawan.bhojanala@outlook.com>
2020-10-30 04:23:16 +0100

    added testingClass

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/TestingClass.java
@@ -0,0 +1,60 @@
+package com.hemebiotech.analytics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TestingClass {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("This class is to test functions in AnalyticsCounter Class");
+		
+		AnalyticsCounter testAC = new AnalyticsCounter();
+		
+		
+		System.out.println("Now testimg  AlphabetizeList method ");
+		
+		List<String> myList = new ArrayList<String>();
+		
+		myList = testAC.alphabetizeList(getList());
+	 	
+	 	for(String e: myList) {
+	 		System.out.println(e);
+	 	}
+	 	
+	 	System.out.println("If you see the list is sorted in alphabetical order the test has succeded");
+	 	
+	 	System.out.println("=============================================");
+	 	
+	 	System.out.println("Now testing the OccurrencesList Method");
+	 	
+	 	
+	 	HashMap<String,Integer> myMap = new HashMap<String,Integer>();
+	 	
+	 	myMap = testAC.occurrencesList(getList());
+	 	
+	 	for(HashMap.Entry<String,Integer> e : myMap.entrySet()) {
+	 		System.out.println(e.getKey() + " : " + e.getValue());
+	 	}
+		
+	 	System.out.println("If you see 2 X Headache, 2 X Fever and 1 X Bleeding the test passes");
+	}
+	
+	
+	static List<String>  getList(){
+		
+		List<String> testList = new ArrayList<String>();
+		
+		testList.add("Fever");
+		testList.add("Fever");
+		testList.add("Headache");
+		testList.add("Headache");
+		testList.add("Bleeding");
+		
+		
+		return testList;
+	}
+
+}
